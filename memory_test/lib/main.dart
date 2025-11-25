c32cb5c95e2615637aebb7c4d6f5d0986fa8596a
Thomas Burkhart <thomas@burkharts.net>
2025-11-25 13:46:48 -0500

    Remove generated test folder from example

--- memory_test/lib/main.dart
+++ memory_test/lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:listen_it_example/model.dart';
+import 'package:memory_test_example/model.dart';
 
 void main() {
   runApp(MyApp());
